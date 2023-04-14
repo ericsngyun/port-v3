c3eb174f5f5bb26f7ddab02ad4bdccbb28031c17
Eric Yun <yunseric@gmail.com>
2023-04-14 11:48:00 -0700

    ooh styling

--- app/page.js
+++ app/page.js
@@ -2,12 +2,27 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 
+function LoadingScreen() {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <main>
-      <div className = "p-8">
-        <h1>TEST</h1>
+      <div className = "p-8 h-screen flex justify-center place-items-center">
+        <div className = "flex-col">
+          <h1 className = "glow text-4xl cursor-pointer text-center">
+            Eric Yun
+          </h1>
+          <h2 className = "glow text-2xl">
+            Software Engineer
+          </h2>
+        </div>
       </div>
     </main>
   )
